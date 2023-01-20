e0ee790ccedb012727134ae4f1faefc355aa2d4f
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-20 18:41:05 +0530

    [Piyush] added to create a linked list to add data

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace LinkedListProblem
+{
+	public class LinkedList
+	{
+		internal Node head;
+
+		//creating add method to add the data in linked list sequentially
+		internal void Add(int data)
+		{
+			Node node = new Node(data);
+			if (this.head == null)
+				this.head = node;
+			else
+			{
+				Node temp = head;
+				while (temp.next != null)
+				{
+					temp = temp.next;
+				}
+				temp.next = node;
+			}
+			Console.WriteLine("{0} inserted into the linked list", node.data);
+		}
+		//displaying the data from linked list
+		internal void Display()
+		{
+			Node temp = this.head;
+			if (temp == null)
+			{
+				Console.WriteLine("LinkedList is empty");
+				return;
+			}
+			while (temp != null)
+			{
+				Console.WriteLine(temp.data + " ");
+				temp = temp.next;
+			}
+		}
+	}
+}
+
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using LinkedListProblem;
+
+internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Linked List Problem");
+        Console.WriteLine("\n1.CreateLinkedList");
+        LinkedList list = new LinkedList();
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Creating a linked list to add data");
+                list.Add(56);
+                list.Add(30);
+                list.Add(70);
+                list.Display();
+                break;
+            default:
+                Console.WriteLine("Please enter correct option ");
+                break;
+        }
     }
 }
